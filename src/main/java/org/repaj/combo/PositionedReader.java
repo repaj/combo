57e38ac09961605e4999b2de2896e8074c4101ee
Konrad <konrad.kleczkowski@hotmail.com>
2017-07-20 17:44:00 +0200

    reader support

---
+++ src/main/java/org/repaj/combo/PositionedReader.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2017 Konrad Kleczkowski
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.repaj.combo;
+
+import java.io.Reader;
+
+/**
+ * @author Konrad Kleczkowski
+ */
+public class PositionedReader {
+    private int position;
+    private Reader reader;
+
+    public PositionedReader(int position, Reader reader) {
+        this.position = position;
+        this.reader = reader;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public Reader getReader() {
+        return reader;
+    }
+}
